e3208d5ce066a690e2f6e7a2e747fd1293eb78d3
TSandile <Sandile.Tshabalala11@gmail.com>
2026-03-05 16:09:49 +0200

    Modify Product Component

--- SpringSecurity/src/main/java/com/example/SpringSecurity/component/Product.java
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/component/Product.java
@@ -1,4 +1,18 @@
 package com.example.SpringSecurity.component;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+
+    private int id;
+    private String name;
+    private Double price;
+    private int quantity;
 }
